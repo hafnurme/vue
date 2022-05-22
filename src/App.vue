f025b60df8e43270ed113eba6d2e566cb8b3e153
hafid <itsmaehere@gmail.com>
2022-05-22 11:42:14 +0700

    first

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,43 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+  <button @click="click">Click</button>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+
+  },
+  data() {
+    return {
+      data: new FormData()
+    }
+  },
+  mounted() {
+    // dataAdd()
+    login()
+    async function login() {
+      try {
+        const response = await axios.post('http://127.0.0.1:8000/api/v1/auth/login', {
+          body: {
+            id_card_number: 1234567890123456,
+            password: '123abc'
+          }
+        });
+        console.log(response.data)
+      } catch (err) {
+        console.log(err)
+      }
+    }
+  },
+  methods: {
+    click() {
+      console.log(this.data)
+    }
   }
 }
 </script>
